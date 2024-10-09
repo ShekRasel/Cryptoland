ce578a72a55783a344b05bd2390dd24c8235db0b
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2024-10-09 23:52:12 +0600

    update,

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@ import Features from '../components/Features'
 import TeamMate from '../components/TeamMate'
 import PromoVideo from '../components/PromoVideo'
 import Testimonials from '../components/Testimonials'
+import Blog from '../components/Blog'
+import Faq from '../components/Faq'
+import SetUpAndStarted from '../components/SetUpAndStarted'
 function Home() {
   return (
     <div className=''>
@@ -31,50 +34,27 @@ function Home() {
 
 
         <div className=' bg-white 2xl:px-72 items-center justify-center flex flex-col  mt-24'>
-          <h2 className='text-center text-blue-600 text-xl font-semibold'>Why Choose Us</h2>
-          <h1 className='text-center text-indigo-800 text-4xl font-semibold mt-6 '>Our Features</h1>
-          <p className='mt-6 font-semibold text-gray-500 text-center w-full px-2 md:px-6  lg:w-1/2'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta optio deserunt rerum ipsa, voluptate doloribus.</p>
+          <div className='flex justify-center'>
+              <div className='lg:w-1/2'>
+                <h2 className='text-center text-blue-600 text-xl font-semibold'>Why Choose Us</h2>
+                <h1 className='text-center text-indigo-800 text-4xl font-semibold mt-4 '>Our Features</h1>
+                <p className='mt-6 font-semibold text-gray-500 text-center w-full  md:px-6'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta optio deserunt rerum ipsa, voluptate doloribus.</p>
+              </div>
+          </div>
         </div>
         <Features/>
 
-        <div className='bg-slate-50 px-4 md:px-8 lg:px-12 xl:px-16 2xl:px-72 pt-20 pb-20'>
-          <h3 className='text-blue-600 font-semibold text-center text-lg'>Ready To Get Started?</h3>
-          <h1 className='text-3xl font-semibold text-indigo-950 mt-4 text-center'>3 Steps to Start</h1>
-          <p className='text-gray-500 mt-4 text-center font-semibold'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.</p>
-
-
-          <div className='text-indigo-900 lg:flex justify-between mt-16 font-bold gap-10 '>
-            <div className='flex flex-col lg:flex-row  mt-10 lg:mt-0 gap-6 items-center'>
-              <span className='rounded-full px-6 py-5 bg-white font-extrabold hover:bg-blue-600 hover:text-white border'>01</span>
-              <div classname=''>
-                <h1 className='text-xl text-center pb-2 lg:pb-0'>Create Your Wallet</h1>
-                <p className='text-gray-500 text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos consectetur tidio.</p>
-              </div>
-            </div>
-            <div className='flex flex-col lg:flex-row  mt-10 lg:mt-0 gap-6 items-center'>
-              <span className='rounded-full px-6 py-5 bg-white font-extrabold hover:bg-blue-600 hover:text-white border'>02</span>
-              <div classname=''>
-                <h1 className='text-xl text-center pb-2 lg:pb-0'>Make Payment</h1>
-                <p className='text-gray-500 text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos consectetur tidio.</p>
-              </div>
-            </div>
-            <div className='flex flex-col lg:flex-row  mt-10 lg:mt-0 gap-6 items-center'>
-              <span className='rounded-full px-6 py-5 bg-white font-extrabold hover:bg-blue-600 hover:text-white border'>03</span>
-              <div classname=''>
-                <h1 className='text-xl text-center pb-2 lg:pb-0'>Buy & Sell Coins</h1>
-                <p className='text-gray-500 text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos consectetur tidio.</p>
-              </div>
-            </div>
-          </div>
-
-
-        </div>
+        <SetUpAndStarted/>
 
         <TeamMate/>
 
         <PromoVideo/>
 
         <Testimonials/>
+
+        <Blog/>
+
+        <Faq/>
         
     </div>
   )
