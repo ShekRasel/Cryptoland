0b1ec842ea6ce9b11c5f0bf1ed671c4d70bac0b6
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2024-10-10 23:19:07 +0600

    link added for password reset page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,19 +32,18 @@ function Navbar() {
     window.addEventListener('scroll',changeBgColor);
 
   return (
-    <nav className={`  font-semibold fixed w-full z-50`} id='nav'>
+    <nav className={`  font-semibold fixed w-full z-50 `} id='nav'>
         
-        <div className= {` px-3 md:px-8 lg:px-12 xl:px-16 2xl:px-72 py-4 lg:py-0 ${bgColor ? 'bg-white transition-all duration-500 z-50 ease-out text-indigo-800 w-full':'transition-all duration-500 ease-out text-white w-full'} w-full`}>
+        <div className= {` px-3 md:px-8 lg:px-12 xl:px-16 2xl:px-72 py-4 lg:py-0 shadow-md ${bgColor ? 'bg-white transition-all duration-500 z-50 ease-out text-indigo-800 w-full':'transition-all duration-500 ease-out text-white w-full'} w-full`}>
 
             <div className='flex justify-between items-center'>
-                
                 <div className=''>
                     <Link to={'/'}>
                         <img src={bgColor ? 'logo/logo.svg' : 'logo/white-logo.svg'}  className='h-9'/>
                     </Link>
                     
                 </div>
-
+                {/*larger Screen nav start*/}
                 <div className='hidden lg:block'>
                     <ul className='flex gap-10 items-center'>
                         <li>
@@ -62,15 +61,15 @@ function Navbar() {
 
                         <li onMouseEnter={showHidePagesDiv} onMouseLeave={showHidePagesDiv} className=''>
                             <div className='py-8'>
-                                <Link>Pages</Link>
+                                <h1 className=' cursor-pointer'>Pages</h1>
                                 {pagesDiv ? 
                                     <div className='mt-3 absolute pt-4 shadow-lg'>
                                         <div className={` rounded-md bg-white transition-all ease-out duration-500 delay-1000 ${pagesDiv ? ' translate-y-0 opacity-100':' -translate-y-5 opacity-0'}`}>
                                             <ul className='text-gray-500  font-semibold flex flex-col gap-4 py-4 pr-20 pl-10'>
-                                                <li className='hover:text-indigo-700'><Link>About Us</Link></li>
+                                                <li className='hover:text-indigo-700'><Link to={'about'}>About Us</Link></li>
                                                 <li className='hover:text-indigo-700'><Link to={'signin'}>Sign In</Link></li>
                                                 <li className='hover:text-indigo-700'><Link to={'signup'}>Sign UP</Link></li>
-                                                <li className='hover:text-indigo-700'><Link>Reset Password</Link></li>
+                                                <li className='hover:text-indigo-700'><Link to={'passwordReset'}>Reset Password</Link></li>
                                                 <li className='hover:text-indigo-700'><Link>Mail Sussess</Link></li>
                                                 <li className='hover:text-indigo-700'><Link>404 Error</Link></li>
                                             </ul>
@@ -109,6 +108,7 @@ function Navbar() {
                    
             
                 </div>
+                {/*larger Screen nav end*/}
 
                 <div className='lg:hidden' onClick={changeHamAndShowMenu}>
                     <span className='text-3xl'>
@@ -121,13 +121,10 @@ function Navbar() {
                         Get Started
                     </button>
                 </div>
-
             </div>
-
-
-
         </div>
-
+        
+        {/*mobile and tab Screen nav start*/}
         <div className={`px-3 md:px-8 mt-1 lg:hidden overflow-hidden ease-out transition[height] duration-500 ${hamBurger ? 'max-h-96':'max-h-0'}`}>
             <div className='flex justify-center bg-white py-4 rounded-md'>
                 <ul className='flex flex-col gap-8'>
@@ -144,9 +141,7 @@ function Navbar() {
                                 >About</NavLink>
                         </li>
                         <li>
-                            <NavLink 
-                                className={({isActive})=>isActive ? 'text-blue-500 font-extrabold':''}
-                                to={'pages'}>Pages</NavLink>
+                            <h1 >Pages</h1>
                         </li>
                         <li>
                             <NavLink 
@@ -162,6 +157,7 @@ function Navbar() {
                 </ul>
             </div>
         </div>
+        {/*mobile and tab Screen nav end*/}
     </nav>
   )
 }
