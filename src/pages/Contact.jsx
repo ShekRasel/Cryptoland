e5a5ed91288ae1e14cc4094a0d6c54d7a1b3b3cf
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2024-10-13 16:54:54 +0600

    property mod

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -39,7 +39,7 @@ function Contact() {
         </div>
 
         <div className='mt-20  p-3 rounded-md bg-white' >
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58308.814657324656!2d90.36575595671752!3d24.020451542921226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755dafdd8aa72a1%3A0xe3a23793cb030fdb!2sGazipur!5e0!3m2!1sen!2sbd!4v1728551605029!5m2!1sen!2sbd" className='w-full h-96' allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58308.814657324656!2d90.36575595671752!3d24.020451542921226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755dafdd8aa72a1%3A0xe3a23793cb030fdb!2sGazipur!5e0!3m2!1sen!2sbd!4v1728551605029!5m2!1sen!2sbd" className='w-full h-96' allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
     </div>
