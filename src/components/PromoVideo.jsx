329083a21e3aae95b8c956f49b4b4ea3eeac32c7
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2024-10-14 00:12:44 +0600

    video add

--- src/components/PromoVideo.jsx
+++ src/components/PromoVideo.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoPlayOutline } from "react-icons/io5";
+import { RxCross1 } from "react-icons/rx";
 
 function PromoVideo() {
+    const [play , setIsPlay ] = useState(false);
+    const playVideo = ()=>{
+        setIsPlay(!play);
+    }
   return (
-        <div className='px-3 md:px-8 lg:px-12 xl:px-16 2xl:px-72 mt-24 bg-indigo-900 h-96 mb-72 pt-16'>
-            <div className='mt-16 rounded-3xl p-8 bg-indigo-600 blu'>
-                    <div className='bg-white rounded-3xl opacity-100 pt-20 pb-20 border'>
+    <div className='mb-96 relative'>
+        <div className='px-3  bg-[#24126A] h-96 relative border md:px-8 lg:px-12 xl:px-16 2xl:px-72 mt-24 '>
+            <div className=' rounded-t-3xl p-8 bg-[#41327C] h-64 mt-[126px] border-gray-400 border'>  
+            </div> 
+
+            <div className='bg-white rounded-3xl pt-4 pb-4 md:pt-20 md:pb-20 border absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-10/12 md:w-5/6 xl:w-4/6 mt-52 shadow-md px-4'>
                         <h1 className='text-blue-600  text-center text-lg'>Create your own experience</h1>
                         <h1 className='text-4xl mt-5 text-indigo-900 font-bold text-center'>Watch our Intro Video</h1>
                         <p className='text-gray-500 mt-6 text-center'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form</p>
-                        <div className='mt-20 flex justify-center'>
-                                <span className='text-2xl border rounded-full bg-blue-600 p-8 text-white animate-pulse'>
-                                    <IoPlayOutline className=''/>
-                                </span>
+                        <div className='mt-20 flex justify-center '>
+                                <div className='flex justify-center border rounded-full p-3'>
+                                    <span className='text-2xl border rounded-full bg-blue-600 p-8 text-white hover:bg-indigo-900 animate-pulse'>
+                                        <IoPlayOutline className='' onClick={playVideo}/>
+                                    </span>
+                                </div>
                         </div>
-                    </div>
             </div>
         </div>
+
+    {play && 
+        <div className='h-screen bg-black fixed  top-0 right-0 left-0 z-50 opacity-90 w-full transition-all ease-out duration-500'>
+            <span className='text-white right-0 absolute text-4xl animate-pulse mr-3 mt-3' ><RxCross1 onClick={playVideo}/></span>
+
+            <div className='border h-full justify-center items-center flex flex-col'>
+            <iframe 
+                    className='w-1/2 h-1/2' 
+                    src="https://www.youtube.com/embed/QTyzyP2Afys?autoplay=1&mute=1" 
+                    title="YouTube video player" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerPolicy="strict-origin-when-cross-origin" 
+                    allowFullScreen
+                ></iframe>
+            </div>
+        </div>}
+    </div>
   )
 }
 
